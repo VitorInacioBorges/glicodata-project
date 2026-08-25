6e7d48126b80e4dbe396cf660e6c93e9e65d4426
VitorInacioBorges <vitorinacioborges@gmail.com>
2026-08-25 16:41:57 -0300

    feat(ui): add professional editing page

---
+++ glicodata/resources/views/ubs/professionals/edit.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.app')
+
+@section('title', 'Editar conta individual')
+@section('protected-navigation', 'true')
+
+@section('content')
+    <main id="conteudo" class="gd-page">
+        <p class="gd-eyebrow">Equipe</p>
+        <h1 class="gd-heading">Editar {{ $professional->name }}</h1>
+
+        <section class="gd-panel gd-form-panel mt-4">
+            <form method="POST" action="{{ route('ubs.professionals.update', $professional) }}">
+                @csrf
+                @method('PUT')
+                @include('ubs.professionals._form')
+            </form>
+        </section>
+    </main>
+@endsection
