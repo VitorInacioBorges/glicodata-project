b8e0eed8475da45dde0d293e2a4e018680220e35
VitorInacioBorges <vitorinacioborges@gmail.com>
2026-08-25 17:42:33 -0300

    refactor(validation): remove unused email validation helpers

--- glicodata/app/Utils/ValidateUtils.php
+++ glicodata/app/Utils/ValidateUtils.php
@@ -2,7 +2,6 @@
 
 namespace App\Utils;
 
-use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Str;
 use Illuminate\Validation\ValidationException;
 
@@ -20,25 +19,5 @@ trait ValidateUtils
                 'id' => ['O id informado deve ser um UUID valido.'],
             ]);
         }
-    }
-
-    /**
-     * @throws ValidationException
-     */
-    private function validateEmail(string $email): void
-    {
-        $validator = Validator::make(
-            ['email' => trim($email)],
-            ['email' => ['required', 'email:rfc', 'max:255']]
-        );
-
-        if ($validator->fails()) {
-            throw new ValidationException($validator);
-        }
-    }
-
-    private function normalizeEmail(string $email): string
-    {
-        return Str::lower(trim($email));
     }
 }
