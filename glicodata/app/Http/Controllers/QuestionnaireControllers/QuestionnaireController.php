cc2b9a102f820c1a9770d15a1290be65789d85b7
VitorInacioBorges <vitorinacioborges@gmail.com>
2026-08-25 16:41:27 -0300

    feat(questionnaire): expose published questionnaire versions

---
+++ glicodata/app/Http/Controllers/QuestionnaireControllers/QuestionnaireController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers\QuestionnaireControllers;
+
+use App\Http\Controllers\Controller;
+use App\Models\QuestionnaireVersionModel;
+use App\Services\QuestionnaireServices\QuestionnaireService;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\Gate;
+
+class QuestionnaireController extends Controller
+{
+    public function __construct(
+        private readonly QuestionnaireService $service,
+    ) {}
+
+    public function current(): JsonResponse
+    {
+        Gate::authorize('viewAny', QuestionnaireVersionModel::class);
+
+        return response()->json($this->service->currentPublished());
+    }
+
+    public function show(string $id): JsonResponse
+    {
+        $version = $this->service->currentPublished($id);
+        Gate::authorize('view', $version);
+
+        return response()->json($version);
+    }
+}
