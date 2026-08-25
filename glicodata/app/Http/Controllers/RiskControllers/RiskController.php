a35d1e1b651216c594bdc38bdce50be866e710ba
VitorInacioBorges <vitorinacioborges@gmail.com>
2026-08-25 17:42:11 -0300

    feat(api): return read-only risk resources

--- glicodata/app/Http/Controllers/RiskControllers/RiskController.php
+++ glicodata/app/Http/Controllers/RiskControllers/RiskController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\RiskControllers;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\CommonRequests\PaginationRequest;
+use App\Http\Resources\RiskResource;
 use App\Models\RiskModel;
 use App\Services\RiskServices\RiskService;
 use App\Support\TenantContext;
@@ -12,19 +13,17 @@ use Illuminate\Support\Facades\Gate;
 
 class RiskController extends Controller
 {
-    public function __construct(
-        protected RiskService $service,
-        protected TenantContext $tenant,
-    ) {}
+    public function __construct(protected RiskService $service, protected TenantContext $tenant) {}
 
     public function index(PaginationRequest $request): JsonResponse
     {
         Gate::authorize('viewAny', RiskModel::class);
-
-        return response()->json($this->service->getRisksForUbs(
+        $collection = RiskResource::collection($this->service->getRisksForUbs(
             $request->perPage(),
             $this->tenant->ubsId($request->user()),
         ));
+
+        return response()->json($collection->response()->getData(true));
     }
 
     public function show(string $id): JsonResponse
@@ -32,6 +31,6 @@ class RiskController extends Controller
         $risk = $this->service->getRiskById($id);
         Gate::authorize('view', $risk);
 
-        return response()->json($risk);
+        return response()->json(RiskResource::make($risk));
     }
 }
