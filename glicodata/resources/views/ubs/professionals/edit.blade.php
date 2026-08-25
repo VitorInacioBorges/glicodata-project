6f1e79a184023ab7a4d11ada63240daf3e0a9377
VitorInacioBorges <vitorinacioborges@gmail.com>
2026-08-25 17:42:42 -0300

    feat(ui): edit professional reference fields

--- glicodata/resources/views/ubs/professionals/edit.blade.php
+++ glicodata/resources/views/ubs/professionals/edit.blade.php
@@ -1,12 +1,12 @@
 @extends('layouts.app')
 
-@section('title', 'Editar conta individual')
+@section('title', 'Editar profissional')
 @section('protected-navigation', 'true')
 
 @section('content')
     <main id="conteudo" class="gd-page">
         <p class="gd-eyebrow">Equipe</p>
-        <h1 class="gd-heading">Editar {{ $professional->name }}</h1>
+        <h1 class="gd-heading">Editar {{ $professional->first_name }}</h1>
 
         <section class="gd-panel gd-form-panel mt-4">
             <form method="POST" action="{{ route('ubs.professionals.update', $professional) }}">
