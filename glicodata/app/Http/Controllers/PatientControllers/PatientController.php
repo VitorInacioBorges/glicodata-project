c40c8a0c6a36dbfbfc2d59cafe10e22385eaa222
VitorInacioBorges <vitorinacioborges@gmail.com>
2026-08-25 17:42:10 -0300

    feat(api): return minimized patient resources

--- glicodata/app/Http/Controllers/PatientControllers/PatientController.php
+++ glicodata/app/Http/Controllers/PatientControllers/PatientController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\CommonRequests\PaginationRequest;
 use App\Http\Requests\PatientRequests\StorePatientRequest;
 use App\Http\Requests\PatientRequests\UpdatePatientRequest;
+use App\Http\Resources\PatientResource;
 use App\Models\PatientModel;
 use App\Services\PatientServices\PatientService;
 use App\Support\TenantContext;
@@ -14,19 +15,17 @@ use Illuminate\Support\Facades\Gate;
 
 class PatientController extends Controller
 {
-    public function __construct(
-        protected PatientService $service,
-        protected TenantContext $tenant,
-    ) {}
+    public function __construct(protected PatientService $service, protected TenantContext $tenant) {}
 
     public function index(PaginationRequest $request): JsonResponse
     {
         Gate::authorize('viewAny', PatientModel::class);
-
-        return response()->json($this->service->getPatientsForUbs(
+        $collection = PatientResource::collection($this->service->getPatientsForUbs(
             $request->perPage(),
             $this->tenant->ubsId($request->user()),
         ));
+
+        return response()->json($collection->response()->getData(true));
     }
 
     public function show(string $id): JsonResponse
@@ -34,7 +33,7 @@ class PatientController extends Controller
         $patient = $this->service->getPatientById($id);
         Gate::authorize('view', $patient);
 
-        return response()->json($patient);
+        return response()->json(PatientResource::make($patient));
     }
 
     public function store(StorePatientRequest $request): JsonResponse
@@ -42,17 +41,17 @@ class PatientController extends Controller
         $ubsId = $this->tenant->ubsId($request->user());
         Gate::authorize('create', [PatientModel::class, $ubsId]);
 
-        return response()->json($this->service->createPatient([
+        return response()->json(PatientResource::make($this->service->createPatient([
             ...$request->validated(),
             'ubs_id' => $ubsId,
-        ]), 201);
+        ])), 201);
     }
 
     public function update(UpdatePatientRequest $request, string $id): JsonResponse
     {
         Gate::authorize('update', $this->service->getPatientById($id));
 
-        return response()->json($this->service->updatePatient($id, $request->validated()));
+        return response()->json(PatientResource::make($this->service->updatePatient($id, $request->validated())));
     }
 
     public function destroy(string $id): JsonResponse
